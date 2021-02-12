80ffe33e1610b3c8bdeeac996c2b29f7b1420874
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-02-12 23:00:25 +0000

    Add files via upload

---
+++ Site1/on_files/on.js
@@ -0,0 +1,7 @@
+// Created by iWeb 3.0.4 local-build-20210212
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('on_files/onMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
