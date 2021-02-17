afd76c6a278b116e620aab29411d8a14a9be3218
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-02-17 15:07:36 +0000

    Delete Site1 directory

--- Site1/off_files/off.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20210212
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('off_files/offMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
