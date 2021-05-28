6cbfbf7ae8c1d29482d66ea28985a09d1554cf5a
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-05-28 02:04:12 +0100

    Add files via upload

--- Site1/Slow_Process_O_files/Slow_Process_O.js
+++ Site1/Slow_Process_O_files/Slow_Process_O.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.4 local-build-20210217
+// Created by iWeb 3.0.4 local-build-20210528
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Slow_Process_O_files/Slow_Process_OMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');performPostEffectsFixups()}
