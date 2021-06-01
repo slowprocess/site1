f7cd85822ecfa9a91c399a7f20c49dc90347e367
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-06-01 01:51:12 +0100

    Delete Site1 directory

--- Site1/Slow_Process_I_files/Slow_Process_I.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20210217
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Slow_Process_I_files/Slow_Process_IMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
--- Site1/Slow_Process_O_files/Slow_Process_O.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20210528
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Slow_Process_O_files/Slow_Process_OMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');performPostEffectsFixups()}
