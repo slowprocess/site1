9cfbeef3f422a1f2ba94a01650e0c2a4f6d15b02
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-06-17 00:02:36 +0100

    Delete Slow_Process directory

--- Slow_Process/Slow_Process_2_files/Slow_Process_2.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20210616
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Slow_Process_2_files/Slow_Process_2Moz.css')
-fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
