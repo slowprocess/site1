91d4411ef00da5f2b92e4d757de5f76665958d7a
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-09-16 22:53:07 +0100

    Delete Slow_Process directory

--- Slow_Process/Play_2_files/Play_2.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20210916
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Play_2_files/Play_2Moz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
