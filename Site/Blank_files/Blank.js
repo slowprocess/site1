1dfa973470fa21d90d25750a6757223d17b128a8
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-09-16 22:53:43 +0100

    Add files via upload

---
+++ Site/Blank_files/Blank.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20210916
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Blank_files/BlankMoz.css')
+detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
