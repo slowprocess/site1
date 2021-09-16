1cc86743a59132ef82d9c0577dd809567a31f9d0
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-09-16 22:21:48 +0100

    Add files via upload

---
+++ Slow_Process/Slow_Process_files/Slow_Process.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20210916
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.03,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Slow_Process_files/Slow_ProcessMoz.css')
+fixupIECSS3Opacity('id1');applyEffects()}
