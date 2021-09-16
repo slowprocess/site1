40796f31edf8da5ec1b5c8a70fd712bc5309a5c8
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-09-16 22:16:34 +0100

    Delete Slow_Process directory

--- Slow_Process/Slow_Process_3_files/Slow_Process_3.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20210617
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Slow_Process_3_files/Slow_Process_3Moz.css')
-applyEffects()}
--- Slow_Process/Slow_Process_files/Slow_Process.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20210617
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Slow_Process_files/Slow_ProcessMoz.css')
-applyEffects()}
