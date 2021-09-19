7358ffdd2d8a0f656e33fec1f61c555929bcc17f
slowprocess <78941527+slowprocess@users.noreply.github.com>
2021-09-19 23:30:34 +0100

    Add files via upload

---
+++ Slow_Process_A/Slow_Process_files/Slow_Process.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20210919
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWStrokeParts([{rect:new IWRect(-1,1,2,68),url:'Slow_Process_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Slow_Process_files/stroke_1.png'},{rect:new IWRect(1,-1,2008,2),url:'Slow_Process_files/stroke_2.png'},{rect:new IWRect(2009,-1,2,2),url:'Slow_Process_files/stroke_3.png'},{rect:new IWRect(2009,1,2,68),url:'Slow_Process_files/stroke_4.png'},{rect:new IWRect(2009,69,2,2),url:'Slow_Process_files/stroke_5.png'},{rect:new IWRect(1,69,2008,2),url:'Slow_Process_files/stroke_6.png'},{rect:new IWRect(-1,69,2,2),url:'Slow_Process_files/stroke_7.png'}],new IWSize(2010,70))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Slow_Process_files/Slow_ProcessMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixupIECSS3Opacity('id1');fixupIECSS3Opacity('id2');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');fixupIECSS3Opacity('id4');applyEffects()}
