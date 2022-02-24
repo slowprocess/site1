49ce5d0730832a7b3719d15ac1a4970703188a1e
slowprocess <78941527+slowprocess@users.noreply.github.com>
2022-02-24 00:55:11 +0000

    Delete 1.1 directory

--- 1.1/Play_again_files/Play_again.js
+++
@@ -1,7 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20220224
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Play_again_files/Play_againMoz.css')
-fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
