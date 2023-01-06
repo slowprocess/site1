fcf87bcd479111d81cf70910446de3b616662e00
slowprocess <78941527+slowprocess@users.noreply.github.com>
2023-01-06 01:44:40 +0000

    Delete 1.2 directory

--- 1.2/SubtleRadio29_12_22_files/SubtleRadio29_12_22.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20230106
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('SubtleRadio29_12_22_files/SubtleRadio29_12_22Moz.css')
-detectBrowser();Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
